8132d29df3fad5f37fee9cad4f88b51df19accd9
bonjouralbertlee@gmail.com <herodotuslee@gmail.com>
2018-08-19 16:59:57 -0700

    ask

--- part1/server.js
+++ part1/server.js
@@ -1,10 +1,59 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+//
+app.use(bodyParser.urlencoded({extended: true}));
+
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+// Challenge 1:
+// Create a GET route for "/hello" that returns the response "He
+// llo!".
+
+app.get('/hello', function(req, res) {
+  res.send("Hello");
 });
+
+// Challenge 2:
+// Create a POST route for "/create/:name" that returns a json object that looks l
+// ike this: { "id":1, "name":"the name they pass in the route" }
+
+app.post('/create/:name', function(req, res) {
+   let guest = {"id":1,"name":req.params.name}
+   console.log(guest)
+   res.send(guest)
+});
+
+
+// Challenge 3:
+// Create a GET route for "/" that returns the index.html file.
+
+app.get('/',(req,res)=>{
+  // res.render("/index.html")
+  res.sendFile(__dirname+'/index.html')
+});
+
+// Challenge 4:
+// Create a GET route for "/verify/:age" that checks if the age is greater than 13.
+//  If it is then it sends a 200 status response (for all good). If it is not great
+//  er than 13 then send a 403 status code. For more information on what status cod
+//  es mean refer to this - https://developer.mozilla.org/en-US/docs/Web/HTTP/Sta
+//  tus
+
+
+app.get('/verify/:age',function(req,res){
+  if(req.params.age>13){
+    res.sendStatus(200)
+
+  }else{
+    res.sendStatus(403)
+  }}
+)
+
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,15 +1,82 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
-});
+//Part two
+// Challenge 1:
+// Create a POST route for "/create/:name/:age" that creates an object that looks
+// like this: { "name": "troy", "age": 20 } Then take that object and insert it in
+// to storage.json
 
-app.use(function(req, res) {
-  res.sendStatus(404);
-});
 
+app.get('/create/:name/:age',(req,res)=>{
+  let name = req.params.name;
+  let age = req.params.age;
+  let origJson = fs.readFileSync('./storage.json','utf8', (err)=>{
+    if(err){console.log(err);}
+  });
+  origJson = JSON.parse(origJson);
+  let users = origJson.users;
+  if(users.length !== 0 ){
+    var uniq_id = users[users.length-1]['id'] + 1;
+  }else{
+    var uniq_id = 1;
+  }
+  let usr = {
+    'id'   : uniq_id,
+    'name' : name,
+    'age'  : age
+  }
+  origJson.users.push(usr);
+  origJson = JSON.stringify(origJson);
+  fs.writeFile('./storage.json', origJson, (err)=>{
+    if(err){
+      console.log(err);
+    }
+  });
+  res.sendStatus(200);
+})
+
+//
+// Challenge 2:
+// Create a Get route for "/" that returns all of the objects inside storage.json.
+app.get('/' , (req, res)=>{
+  var storage = fs.readFileSync('./storage.json','utf8',(err)=>{
+    console.log(storage);
+  });
+  res.json(storage);
+})
+
+// Challenge 3:
+// Create a Get route for "/:name" that returns the first object in storage.json th
+// at matches the name. If there is no object in storage.json that matches then re
+// turn a 400 status.
+
+app.get('/:id',(req,res)=>{
+  let storage = fs.readFileSync('./storage.json','utf8',(err)=>{
+    console.log(storage);
+  });
+  let result = false ;
+  // for (let key in JSON.parse(storage)){
+  //   result = JSON.parse(storage).users.id == [req.params.id] ? true : false;
+  // }
+  var users = JSON.parse(storage).users ;
+  var user_found ;
+  users.forEach((user)=>{
+    if(user.id == [req.params.id]){
+      result = true
+      user_found = user;
+    }
+  })
+
+  if(result){
+    res.send(`Found user_id : ${user_found.id} \nUser Name: ${user_found.name}`);
+  }else{
+    res.send("NOT FOUND");
+  }
+
+})
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
